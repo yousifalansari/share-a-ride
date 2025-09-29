9a494d6019cba1f70672280e72bad800d202d314
yousifalansari <y.alansari2006@gmail.com>
2025-09-29 08:05:07 +0300

    updated file structure and userSchema

--- models/user.js
+++ models/user.js
@@ -4,11 +4,29 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  contactDetails: {
+    type: String,
+    trim: true,
+  },
+}, {
+  timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);
