fa14ad116477cf1faed1a0c674664fa7ec98a9d0
yousifalansari <y.alansari2006@gmail.com>
2025-09-29 08:20:42 +0300

    init ride routing

---
+++ controllers/ride.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Ride = require('../models/ride');
+const isSignedIn = require('../middleware/is-signed-in.js');
+
+const router = express.Router();
+
+
+
+module.exports = router;
