46d68fe9b50806b948aa8ab4c6d8e45ea60f31dc
yousifalansari <y.alansari2006@gmail.com>
2025-09-29 08:26:47 +0300

    showing all rides regardless of access

--- controllers/ride.js
+++ controllers/ride.js
@@ -4,6 +4,16 @@ const isSignedIn = require('../middleware/is-signed-in.js');
 
 const router = express.Router();
 
+// Show all rides - accessible to all users/guests
+router.get('/', async (req, res) => {
+  try {
+    const rides = await Ride.find().populate('driverId', 'username');
+    res.render('ride/list.ejs', { rides });
+  } catch (error) {
+    res.status(500).send('Error loading rides');
+  }
+});
+
 
 
 module.exports = router;
